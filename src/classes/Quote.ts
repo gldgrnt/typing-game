11218fa0865d131a8bb905781a52aace9848ddab
Gilad Grant <ggrant966@gmail.com>
2021-02-08 14:58:16 +0000

    add getter methods

--- src/classes/Quote.ts
+++ src/classes/Quote.ts
@@ -1,22 +1,63 @@
-import { TQuoteApiDataQuote } from 'services';
-import { Text } from './Text';
-
 export class Quote {
     author: string;
-    text: Text;
     length: number;
+    words: string[];
+    currentIndex = 0;
+    dateFinished = new Date();
+    isFinished = false;
 
-    constructor(apiQuote?: TQuoteApiDataQuote) {
-        const { author, text } = apiQuote || this._defaultApiQuote();
+    constructor(author = '', text = '') {
         this.author = author;
-        this.text = new Text(text);
+        this.words = text.split(' ');
         this.length = text.length;
     }
 
-    private _defaultApiQuote(): TQuoteApiDataQuote {
-        return {
-            author: '',
-            text: '',
-        };
+    getCurrentIndex(): number {
+        return this.currentIndex;
+    }
+
+    getWord(index: number): string {
+        return this.words[index];
+    }
+
+    getFirstWord(): string {
+        if (this.length === 0) {
+            return '';
+        }
+
+        return this.getWord(0);
+    }
+
+    hasNextWord(): boolean {
+        return !!this.words[this.currentIndex + 1];
+    }
+
+    hasPrevWord(): boolean {
+        return !!this.words[this.currentIndex - 1];
+    }
+
+    getNextWord(): string {
+        this.currentIndex === this.length
+            ? this.length
+            : (this.currentIndex += 1);
+        return this.getWord(this.currentIndex);
+    }
+
+    getPrevWord(): string {
+        this.currentIndex === 0 ? 0 : (this.currentIndex -= 1);
+        return this.getWord(this.currentIndex);
+    }
+
+    finishQuote(): void {
+        this.isFinished = true;
+        this.dateFinished = new Date();
+    }
+
+    getFinishedStatus(): boolean {
+        return this.isFinished;
+    }
+
+    toString(): string {
+        return this.words.join(' ');
     }
 }
