5f0a48f7477eae46f23f3d3644e329bdee9b9acf
Gilad Grant <ggrant966@gmail.com>
2021-02-08 14:53:37 +0000

    add placeholder files for historyState

---
+++ src/state/historyState/_types.ts
@@ -0,0 +1,14 @@
+// import { Quote } from 'classes';
+
+// export type THistoryState = {
+//     red: true;
+// };
+
+// export type THistoryContext = [THistoryState, HistoryStateActions];
+
+// export type THistoryStateAction = {
+//     type: ACTIONS;
+//     payload?: Quote | boolean | Error | null;
+// };
+
+export {};
