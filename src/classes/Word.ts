aa2ceb345a458a1ff5997e068fc479f157f45e4c
Gilad Grant <ggrant966@gmail.com>
2021-02-09 17:05:37 +0000

    add static methods

--- src/classes/Word.ts
+++ src/classes/Word.ts
@@ -1,12 +1,46 @@
-export class Word {
-    word;
-    userInput = '';
+export enum WORD_STATUS {
+    COMPLETED,
+    CURRENT,
+    INCOMPLETE,
+}
 
-    constructor(word: string) {
-        this.word = word;
+export class Word {
+    static getStatus(wordIndex: number, currentIndex: number): WORD_STATUS {
+        if (wordIndex < currentIndex) {
+            return WORD_STATUS.COMPLETED;
+        } else if (wordIndex > currentIndex) {
+            return WORD_STATUS.INCOMPLETE;
+        } else {
+            return WORD_STATUS.CURRENT;
+        }
     }
 
-    static compare(a: string, b: string): boolean {
-        return a === b;
+    static isEqual(word: string, correctWord: string): boolean {
+        if (word.length !== correctWord.length) return false;
+        return word === correctWord;
+    }
+
+    static isEqualSoFar(word: string, correctWord: string): boolean {
+        if (word.length >= correctWord.length)
+            return this.isEqual(word, correctWord);
+        return word === correctWord.substr(0, word.length);
+    }
+
+    static getColor(wordA: string, wordB: string, status: WORD_STATUS): string {
+        const { COMPLETED, CURRENT, INCOMPLETE } = WORD_STATUS;
+        // + changes enum to number
+        switch (+status) {
+            case INCOMPLETE:
+                return 'black';
+
+            case CURRENT:
+                return this.isEqualSoFar(wordA, wordB) ? 'orange' : 'red';
+
+            case COMPLETED:
+                return this.isEqual(wordA, wordB) ? 'green' : 'red';
+
+            default:
+                return 'black';
+        }
     }
 }
