9e30b90aafeb5a6d155763b06a39b0926d30b8e7
Gilad Grant <ggrant966@gmail.com>
2021-04-06 15:16:29 +0100

    add providerComposer

--- src/components/App/Main/GameView/GameView.tsx
+++ src/components/App/Main/GameView/GameView.tsx
@@ -1,20 +1,22 @@
-import { GameContextProvider } from './_contexts/GameContext';
+import { ProviderComposer } from 'components/shared';
+import { GameContext, QuoteContext } from './_contexts';
+const { GameContextProvider } = GameContext;
+const { QuoteContextProvider } = QuoteContext;
 import { Quote } from './Quote';
 
 export const GameView: React.FC = () => {
     return (
-        <GameContextProvider>
+        <ProviderComposer
+            contextProviders={[GameContextProvider, QuoteContextProvider]}
+        >
             <h1>This is the Game view</h1>
             <Quote />
-        </GameContextProvider>
+        </ProviderComposer>
     );
 };
 
 /*
 Comoponents:
-    -> Timer
-        -> reset: NOT_STARTED
-        -> start: STARTED
     -> Quote
     -> Input
     -> History sidebar
