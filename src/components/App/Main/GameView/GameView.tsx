8f7b6dc0952abc3e692adbe7388178e58b1fb604
Gilad Grant <ggrant966@gmail.com>
2021-04-14 16:43:05 +0100

    update with new & placeholder components

--- src/components/App/Main/GameView/GameView.tsx
+++ src/components/App/Main/GameView/GameView.tsx
@@ -1,21 +1,43 @@
 import { ProviderComposer } from 'components/shared';
 import { GameContext, QuoteContext } from './_contexts';
-const { GameContextProvider } = GameContext;
+const { GameContextProvider, useGameContext, GAME_STATE } = GameContext;
 const { QuoteContextProvider } = QuoteContext;
+
 import { Quote } from './Quote';
 import { Input } from './Input';
+import { GetQuoteButton } from './GetQuoteButton';
 
-export const GameView: React.FC = () => {
+const contexts = [GameContextProvider, QuoteContextProvider];
+
+export const GameViewInner: React.FC = () => {
+    const gameState = useGameContext().state;
+    // call useGetQuote here to preserve the state
+
+    // Helper booleans
+    const isLoadingQuote = gameState === GAME_STATE.LOADING;
+
     return (
-        <ProviderComposer
-            contextProviders={[GameContextProvider, QuoteContextProvider]}
-        >
+        <>
             <h1>This is the Game view</h1>
+            {isLoadingQuote ? (
+                <h2>Loading</h2>
+            ) : (
+                <div>
+                    <GetQuoteButton />
+                </div>
+            )}
             <Quote />
             <Input />
-        </ProviderComposer>
+            <p>{gameState}</p>
+        </>
     );
 };
+
+export const GameView: React.FC = () => (
+    <ProviderComposer contextProviders={contexts}>
+        <GameViewInner />
+    </ProviderComposer>
+);
 
 /*
 Comoponents:
