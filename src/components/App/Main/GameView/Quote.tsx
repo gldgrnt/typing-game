876d38e2553a5ffd39570863fe3f99eb3197ac10
Gilad Grant <ggrant966@gmail.com>
2021-04-14 16:41:27 +0100

    move show/hide logic up a level

--- src/components/App/Main/GameView/Quote.tsx
+++ src/components/App/Main/GameView/Quote.tsx
@@ -1,22 +1,12 @@
-import { GameContext, QuoteContext } from './_contexts';
-import { TGameState } from './_contexts/GameContext/_types';
-import { useGetQuote } from './_hooks/useGetQuote/useGetQuote';
+import { QuoteContext } from './_contexts';
 
 export const Quote: React.FC = () => {
-    const gameContext = GameContext.useGameContext();
     const quoteContext = QuoteContext.useQuoteContext();
-    const getQuote = useGetQuote();
-
     const quoteText = quoteContext.state.text;
 
     return (
         <div>
-            {gameContext.state === TGameState.LOADING ? (
-                <h2>Loading</h2>
-            ) : (
-                <h2>{quoteText || 'Get a new quote!'}</h2>
-            )}
-            <button onClick={getQuote}>Get quote</button>
+            <h2>{quoteText || 'Get a new quote!'}</h2>
         </div>
     );
 };
